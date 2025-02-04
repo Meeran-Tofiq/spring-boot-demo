e97c9054242bfa474c432c808d9d215b9297a7bf
meeran <101665499+Meeran-Tofiq@users.noreply.github.com>
2025-02-04 15:23:27 +0300

    feat: add getting runs

---
+++ src/main/java/com/example/spring_boot_demo/run/RunRepository.java
@@ -0,0 +1,47 @@
+package com.example.spring_boot_demo.run;
+
+import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class RunRepository {
+    private static final Logger log = LoggerFactory.getLogger(RunRepository.class);
+    private final HashMap<Integer, Run> runs = new HashMap<>();
+
+    List<Run> findAll() {
+        return runs.values().stream().toList();
+    }
+
+    Optional<Run> findById(Integer id) {
+        return Optional.ofNullable(runs.get(id));
+    }
+
+    @PostConstruct
+    private void init() {
+        Run r1 = new Run(
+                1,
+                "Hello",
+                LocalDateTime.now(),
+                LocalDateTime.now().plusHours(3),
+                5,
+                Location.OUTDOOR
+        );
+        Run r2 = new Run(
+                2,
+                "Hello",
+                LocalDateTime.now(),
+                LocalDateTime.now().plusHours(1),
+                5,
+                Location.INDOOR
+        );
+        runs.put(r1.id(), r1);
+        runs.put(r2.id(), r2);
+    }
+}
