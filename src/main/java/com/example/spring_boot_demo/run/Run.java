55436cc1b4dfe4efb2a208ac8b86139df153201f
meeran <101665499+Meeran-Tofiq@users.noreply.github.com>
2025-02-04 17:01:50 +0300

    feat: add custom field validation for run record

--- src/main/java/com/example/spring_boot_demo/run/Run.java
+++ src/main/java/com/example/spring_boot_demo/run/Run.java
@@ -1,13 +1,23 @@
 package com.example.spring_boot_demo.run;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
 import java.time.LocalDateTime;
 
 public record Run(
-        Integer id,
-        String title,
-        LocalDateTime startedOn,
-        LocalDateTime completedOn,
-        Integer miles,
-        Location location
+		Integer id,
+		@NotNull
+		String title,
+		LocalDateTime startedOn,
+		LocalDateTime completedOn,
+		@Positive
+		Integer miles,
+		Location location
 ) {
+	public Run {
+		if (!completedOn.isAfter(startedOn)) {
+			throw new IllegalArgumentException("Completed time of run cannot be before the run starting.");
+		}
+	}
 }
--- src/main/java/com/example/spring_boot_demo/run/RunController.java
+++ src/main/java/com/example/spring_boot_demo/run/RunController.java
@@ -1,6 +1,7 @@
 package com.example.spring_boot_demo.run;
 
 
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,41 +12,41 @@ import java.util.Optional;
 @RequestMapping("/api/runs")
 public class RunController {
 
-    private final RunRepository runRepository;
+	private final RunRepository runRepository;
 
-    public RunController(RunRepository runRepository) {
-        this.runRepository = runRepository;
-    }
+	public RunController(RunRepository runRepository) {
+		this.runRepository = runRepository;
+	}
 
-    @GetMapping("")
-    List<Run> getRuns() {
-        return runRepository.findAll();
-    }
+	@GetMapping("")
+	List<Run> getRuns() {
+		return runRepository.findAll();
+	}
 
-    @GetMapping("/{id}")
-    Run getRunById(@PathVariable("id") Integer id) {
-        Optional<Run> run = runRepository.findById(id);
-        if (run.isEmpty()) {
-            throw new RunNotFoundException();
-        }
-        return run.get();
-    }
+	@GetMapping("/{id}")
+	Run getRunById(@PathVariable("id") Integer id) {
+		Optional<Run> run = runRepository.findById(id);
+		if (run.isEmpty()) {
+			throw new RunNotFoundException();
+		}
+		return run.get();
+	}
 
-    @ResponseStatus(HttpStatus.CREATED)
-    @PostMapping("")
-    void createRun(@RequestBody Run run) {
-        runRepository.create(run);
-    }
+	@ResponseStatus(HttpStatus.CREATED)
+	@PostMapping("")
+	void createRun(@Valid @RequestBody Run run) {
+		runRepository.create(run);
+	}
 
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    @PutMapping("/{id}")
-    void updateRunById(@PathVariable("id") Integer id, @RequestBody Run run) {
-        runRepository.update(id, run);
-    }
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@PutMapping("/{id}")
+	void updateRunById(@Valid @PathVariable("id") Integer id, @RequestBody Run run) {
+		runRepository.update(id, run);
+	}
 
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    @DeleteMapping("/{id}")
-    void deleteRunById(@PathVariable("id") Integer id) {
-        runRepository.delete(id);
-    }
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@DeleteMapping("/{id}")
+	void deleteRunById(@PathVariable("id") Integer id) {
+		runRepository.delete(id);
+	}
 }
